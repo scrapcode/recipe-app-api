74fb662f5065e7059948113cc10d1cc6aa7bbd6d
Cam <ctcecil@gmail.com>
2022-06-16 12:03:12 -0500

    Wait for DB checks

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculator functions
+"""
+
+
+def add(x, y):
+    """Add x and y and return the resut"""
+    return x + y
+
+
+def subtract(x, y):
+    """Subtract x from y and return the result"""
+    return y - x
